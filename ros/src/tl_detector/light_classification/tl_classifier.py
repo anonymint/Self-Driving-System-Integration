7e2120355e3a46634c07cce7c760095f268c78ed
Anonymint <mint.com@gmail.com>
2018-08-14 17:42:20 -0500

    Add my frozen_interence_graph and update classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,36 @@
 from styx_msgs.msg import TrafficLight
+import tensorflow as tf
+import numpy as np
+import datetime
 
 class TLClassifier(object):
-    def __init__(self):
+    def __init__(self, is_site = False):
         #TODO load classifier
-        pass
+        self.is_site = is_site
+        if self.is_site :
+            model_path = "light_classification/model/frozen_inference_graph_real.pb"
+        else:
+            model_path = "light_classification/model/frozen_inference_graph_sim.pb"
+
+        self.graph = tf.Graph()
+        self.threshold = .5
+
+        with self.graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(model_path, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+             # TF Graph is ready
+            self.image_tensor = self.graph.get_tensor_by_name('image_tensor:0')
+            self.detect_boxes = self.graph.get_tensor_by_name('detection_boxes:0')
+            self.detect_scores = self.graph.get_tensor_by_name('detection_scores:0')
+            self.detect_classes = self.graph.get_tensor_by_name('detection_classes:0')
+            self.num_detections = self.graph.get_tensor_by_name('num_detections:0')
+
+        # pass graph session to rest of program
+        self.sess = tf.Session(graph=self.graph)
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -15,5 +42,39 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
+
+        # # FOR TESTING STATIC VALUE USE THIS BLOCK - START
+        # return TrafficLight.UNKNOWN
+        # # FOR TESTING STATIC VALUE USE THIS BLOCK - END
+
+        # # FOR TF CLASSIFIED USE THIS BLOCK - START
+        with self.graph.as_default():
+            image_expanded = np.expand_dims(image, axis=0)
+            start_clock = datetime.datetime.now()
+            (boxes, scores, classes, num) = self.sess.run(
+                [self.detect_boxes, self.detect_scores, self.detect_classes, self.num_detections],
+                feed_dict={self.image_tensor: image_expanded})
+            stop_clock = datetime.datetime.now()
+            print("Classified in", (stop_clock-start_clock).total_seconds())
+
+        # squeeze here from shape (1,10) to (,10)
+        scores = np.squeeze(scores)
+        classes = np.squeeze(classes).astype(np.int32)
+        print('SCORES', scores[0], 'CLASSES', classes[0])
+
+        # color code
+        # {
+        #  1: {'id': 1, 'name': 'Green'},
+        #  2: {'id': 2, 'name': 'Red'},
+        #  3: {'id': 3, 'name': 'Yellow'},
+        #  4: {'id': 4, 'name': 'off'}
+        # }
+        if scores[0] >= self.threshold:
+            if classes[0] == 1:
+                return TrafficLight.GREEN
+            elif classes[0] == 2:
+                return TrafficLight.RED
+            elif classes[1] == 3:
+                return TrafficLight.YELLOW
         return TrafficLight.UNKNOWN
+        # # FOR TF CLASSIFIED USE THIS BLOCK - END
--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -42,7 +42,10 @@ class TLDetector(object):
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
         self.bridge = CvBridge()
-        self.light_classifier = TLClassifier()
+        self.is_site = self.config["is_site"]
+        self.waypoints_2d = None
+        self.waypoints_tree = None
+        self.light_classifier = TLClassifier(self.is_site)
         self.listener = tf.TransformListener()
 
         self.state = TrafficLight.UNKNOWN
@@ -51,8 +54,8 @@ class TLDetector(object):
         self.state_count = 0
         self.light_detection_count = 0
 
-        self.waypoints_2d = None
-        self.waypoints_tree = None
+        # self.waypoints_2d = None
+        # self.waypoints_tree = None
 
         rospy.spin()
 
@@ -131,16 +134,16 @@ class TLDetector(object):
         """
 
         # For testing we can use only
-        return light.state
+        # return light.state
 
-        # if(not self.has_image):
-        #     self.prev_light_loc = None
-        #     return False
-        #
-        # cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
-        #
-        # #Get classification
-        # return self.light_classifier.get_classification(cv_image)
+        if(not self.has_image):
+            self.prev_light_loc = None
+            return False
+
+        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+
+        #Get classification
+        return self.light_classifier.get_classification(cv_image)
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
@@ -158,7 +161,7 @@ class TLDetector(object):
         stop_line_positions = self.config['stop_line_positions']
         if(self.pose):
             car_position = self.get_closest_waypoint(self.pose.pose.position.x, self.pose.pose.position.y)
-            rospy.logwarn("current position {0} {1}".format(self.pose.pose.position.x, self.pose.pose.position.y))
+            # rospy.logwarn("current position {0} {1}".format(self.pose.pose.position.x, self.pose.pose.position.y))
             # rospy.logwarn("lights {0} ".format([(l.pose.pose.position.x, l.pose.pose.position.y, l.state) for l in self.lights]))
             #TODO find the closest visible traffic light (if one exists)
             diff = len(self.waypoints.waypoints)
